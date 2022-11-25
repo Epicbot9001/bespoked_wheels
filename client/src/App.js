e2484124b0877aa9a053db968a792b9adb432ddc
EpicBot9001 <stanley0620@gmail.com>
2022-11-25 15:57:09 -0500

    displayed mysql products table

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import { useState } from "react";
+import Axios from 'axios'
 
 function App() {
+  const [productsList, setProductsList] = useState([]);
+
+  const getProducts = () => {
+    Axios.get("http://localhost:3001/products").then((response) => {
+      setProductsList(response.data)
+    });
+  }
+
   return (
     <div className="App">
-      helloaaaa
+      BeSpoked Wheels
+      <div className = "products">
+        <button onClick={getProducts}>
+          Show Products
+        </button>
+        {productsList.map((val, key) => {
+          return (
+            <div className = "product">
+              <h3>Name: {val.name}</h3>
+              <h3>Manufacturer: {val.manufacturer}</h3>
+              <h3>Style: {val.style}</h3>
+              <h3>Purchase Price: {val.purchasePrice}</h3>
+              <h3>Sale Price: {val.salePrice}</h3>
+              <h3>Qty on Hand: {val.qtyOnHand}</h3>
+              <h3>Commission %: {val.commissionPercentage}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
+const cors = require('cors')
 
+app.use(cors());
+
+//Parse json
+app.use(express.json());
+
+//password is either empty or "password"
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'bespoked_data'
+});
+
+//Makes a route for create so would be localhost:3001/create
+//Take from frontend is req (request), and send something to frontend is res (respond)
+app.post('/create', (req, res) => {
+
+})
+
+app.get('/products', (req, res) => {
+    db.query("SELECT * FROM products", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 app.listen(3001, ()=> {
     console.log("ya server running on port 3001");
