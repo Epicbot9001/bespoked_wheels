c442639f5dc461865af9f88f4fd5db30259a6d6f
EpicBot9001 <stanley0620@gmail.com>
2022-11-26 05:49:29 -0500

    converted to antd and added update functionality

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql2')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
 
-//CORS allows servers specify origins to permit requests from. 
+//CORS allows servers specify origins to permit requests from.
 app.use(cors());
 
 //Parse json
@@ -11,44 +11,62 @@ app.use(express.json());
 
 //database connection
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'password',
-    database: 'bespoked_data'
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "bespoked_data",
 });
 
 //Makes a route for create (localhost:3001/create)
 //Take from frontend is req (request), and send something to frontend is res (respond) through insertion
-app.post('/create', (req, res) => {
-
-})
+app.post("/create", (req, res) => {});
 
 //Makes route for products (localhost:3001/products)
 //Will get (read) data from database
-app.get('/products', (req, res) => {
-    //Select everything from product table
-    db.query("SELECT * FROM products", (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(result)
-        }
-    });
+app.get("/products", (req, res) => {
+  //Select everything from product table
+  db.query("SELECT * FROM products", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.put('/update', (req, res) => {
-    const id = req.body.id;
-    const name = req.body.id;
-    db.query("UPDATE products SET name = ? WHERE id = ?", [name, id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+app.put("/update", (req, res) => {
+  const productID = req.body.productID;
+  const name = req.body.name;
+  const manufacturer = req.body.manufacturer;
+  const style = req.body.style;
+  const purchasePrice = req.body.purchasePrice;
+  const salePrice = req.body.salePrice;
+  const qtyOnHand = req.body.qtyOnHand;
+  const commissionPercentage = req.body.commissionPercentage;
+
+  db.query(
+    "UPDATE products SET name = ?, manufacturer = ?, style = ?, purchasePrice = ?, salePrice = ?, qtyOnHand = ?, commissionPercentage = ? WHERE productID = ?",
+    [
+      name,
+      manufacturer,
+      style,
+      purchasePrice,
+      salePrice,
+      qtyOnHand,
+      commissionPercentage,
+      productID,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
 });
 
 //confirmation of running server
-app.listen(3001, ()=> {
-    console.log("ya server running on port 3001");
-})
+app.listen(3001, () => {
+  console.log("ya server running on port 3001");
+});
